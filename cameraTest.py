cd93a11bd335eef2fde4b5ab367fbc3da5afb0b8
Arjun Kalburgi <askalburgi@gmail.com>
2017-03-19 04:23:15 -0700

    more cleanup, added main()

--- cameraTest.py
+++
@@ -1,9 +0,0 @@
-from picamera import PiCamera
-from time import sleep
-import os
-camera = PiCamera()
-camera.rotation = 270
-camera.start_preview()
-sleep(2)
-camera.capture('/home/pi/nwHacks/try1.jpg')
-camera.stop_preview()
--- ser.py
+++ ser.py
@@ -1,12 +1,13 @@
 import serial
-#import test1
+import main
 
 ser = serial.Serial('/dev/ttyACM0',9600)
 s = [0,1]
 while True:
-        read_serial=ser.readline()
+	read_serial=ser.readline()
 
-        print read_serial
-        if read_serial!="":
-                print "TAKE PICTURE"
-                #PUT func here test1.main()
+	print read_serial
+	if read_serial!="":
+		print "TAKE PICTURE"
+		#PUT func here test1.main()
+		main()
