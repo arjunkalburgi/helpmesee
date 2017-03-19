823d35cdd62adb7a999cc08c7b15a0986ab13616
Arjun Kalburgi <askalburgi@gmail.com>
2017-03-19 04:42:12 -0700

    defining ser as a func

--- ser.py
+++ ser.py
@@ -1,13 +1,14 @@
 import serial
 import main
 
-ser = serial.Serial('/dev/ttyACM0',9600)
-s = [0,1]
-while True:
-	read_serial=ser.readline()
+def ser()
+	ser = serial.Serial('/dev/ttyACM0',9600)
+	s = [0,1]
+	while True:
+		read_serial=ser.readline()
 
-	print read_serial
-	if read_serial!="":
-		print "TAKE PICTURE"
-		#PUT func here test1.main()
-		main()
+		print read_serial
+		if read_serial!="":
+			print "TAKE PICTURE"
+			#PUT func here test1.main()
+			main()
